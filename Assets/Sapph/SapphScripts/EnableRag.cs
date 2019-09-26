a0ce24d9a203b0fe39b5dafe88d12f317e18a2d3
Sapphire Fowler <kylespell101@gmail.com>
2019-09-26 15:06:56 -0700

    Ragdolls

---
+++ Assets/Sapph/SapphScripts/EnableRag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableRag : MonoBehaviour
+{
+    public List<Rigidbody> rigidbodies;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (/*other.gameObject.name == "Cube" || other.gameObject.name.Contains("p3vr")*/!other.gameObject.transform.IsChildOf(gameObject.transform) && other.gameObject.name != "Plane")
+        {
+            foreach (Rigidbody rigi in rigidbodies)
+            {
+                rigi.isKinematic = false;
+            }
+        }
+    }
+}
