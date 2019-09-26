fdf979e8c61a9e1e72ec4cead1eb2b25c84c3234
Ryan Healy <ryan.healy11@gmail.com>
2019-09-26 14:55:16 -0700

    TowerFall

---
+++ Assets/RyansAssets/TowerSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawn : MonoBehaviour
+{
+
+    public GameObject towerLayer;
+    public int towerHeight = 1;
+    public float towerLayerRotation = 10.0f;
+
+    private Quaternion currRotation = Quaternion.identity;
+    private Vector3 currPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currPos = gameObject.transform.position;
+
+        for (int i = 0; i < towerHeight; i++)
+        {
+            Instantiate(towerLayer, currPos, currRotation);
+            currRotation = currRotation * Quaternion.AngleAxis(towerLayerRotation, Vector3.up);
+            currPos += new Vector3(0, 1.00f, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
