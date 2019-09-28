55324102e69f7bcaa33e55da692d7013a976a6fa
Noah Albright <albright.noahk@gmail.com>
2019-09-27 17:41:58 -0700

    added prefab functionality on tower

--- Assets/RyansAssets/TowerSpawn.cs
+++ Assets/RyansAssets/TowerSpawn.cs
@@ -9,6 +9,7 @@ public class TowerSpawn : MonoBehaviour
     public uint towerHeight = 1;
     public float towerLayerRotation = 10.0f;
     public uint bricksPerLayer = 8;
+    public bool flipLayer = false;
 
     private Quaternion currRotation = Quaternion.identity;
     private Vector3 currPos;
@@ -19,15 +20,33 @@ public class TowerSpawn : MonoBehaviour
     {
         currPos = gameObject.transform.position;
 
+        // Adjust height for flippable layer
+        if(flipLayer == true)
+        {
+            currPos += new Vector3(0, 0.5f, 0);
+        }
+
         for (int i = 0; i < towerHeight; i++)
         {
             GameObject layer = Instantiate(towerLayer, currPos, currRotation, transform);
+            // Get LayerSpawn component from new layer object
             LayerSpawn layerSpawn = layer.GetComponent<LayerSpawn>();
             if (layerSpawn != null)
             {
+                //set number of bricks to spawn on the new layer
                 layerSpawn.bricksPerLayer = bricksPerLayer;
             }
-            currRotation = currRotation * Quaternion.AngleAxis(towerLayerRotation, Vector3.up);
+
+            // Flip every other layer
+            if(flipLayer == true && i % 2 == 0)
+            {
+                layer.transform.localRotation = Quaternion.AngleAxis(180, Vector3.right);
+            }
+
+            if (flipLayer == false)
+            {
+                currRotation = currRotation * Quaternion.AngleAxis(towerLayerRotation, Vector3.up);
+            }
             currPos += new Vector3(0, 1.00f, 0);
         }
     }
