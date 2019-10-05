d43b8906c2db05308da69f78df369cef94f245fe
Sapphire Fowler <kylespell101@gmail.com>
2019-10-04 17:30:03 -0700

    Moving textures

---
+++ Assets/Sapph/SapphScripts/ScrollingTextures.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingTextures : MonoBehaviour
+{
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+
+    public float multi = 1.5f;
+
+    Vector2 uvOffset = Vector2.zero;
+
+    private void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(gameObject.transform.localScale.x * multi, gameObject.transform.localScale.z * multi);
+        //gameObject.GetComponent<Renderer>().material.SetTextureScale() = new Vector2(gameObject.transform.localScale.x * multi, gameObject.transform.localScale.z * multi);
+    }
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (gameObject.GetComponent<Renderer>().enabled)
+        {
+            gameObject.GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
